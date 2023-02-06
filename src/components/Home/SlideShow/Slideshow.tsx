0be15fe07393fd55b4457b6dcb75f28983e8f331
solisedwin10 <solisedwin10@gmail.com>
2023-02-06 01:07:10 -0500

    Fixed spacing width size for image and the box component that surrounds it. Replaced Text component with Heading component

--- src/components/Home/SlideShow/Slideshow.tsx
+++ src/components/Home/SlideShow/Slideshow.tsx
@@ -1,34 +1,38 @@
 import React from "react"
-import { Button, Box, Image, Text} from "@chakra-ui/react"
+import { Button, Box, Container, Heading, Image, Text} from "@chakra-ui/react"
 import {SlideFade, useDisclosure } from '@chakra-ui/react'
 
 const Slideshow = () => {
     const { isOpen, onToggle } = useDisclosure()
 
     return (
-      <>
-        <Button onClick={onToggle}>Click Me</Button>
-        <SlideFade in={isOpen} offsetY='20px'>
-          <Box
-            p='40px'
-            color='white'
-            mt='4'
-            bg='teal.500'
-            rounded='md'
-            shadow='md'
-          >  
-          <Image 
-          src='https://bit.ly/dan-abramov' 
-          objectFit='cover'
-          
-          alt='Dan Abramov'
-           />
-        <Text fontSize='xl' color='white' >
-            Scour the internet for books that are customized to your interests. 
-        </Text>
-          </Box>
-        </SlideFade>
-      </>
+      <Container  position='absolute' minW='55%'> 
+            <Button onClick={onToggle}>Click Me</Button>
+            <SlideFade in={isOpen}>
+            <Box
+                p='2.5rem'
+                color='white'
+                mt='7'
+                ml='5%'
+                minW={30}
+                bg='teal.500'
+                rounded='md'
+                shadow='md'
+            >  
+            <Image 
+            width='50%'
+            margin={0}
+            src='https://bit.ly/dan-abramov' 
+            objectFit='cover'            
+            alt='Dan Abramov'
+            />
+            <Heading size='md' mt={5}>
+                Scour the internet for books that are customized to your personal interests. 
+            </Heading>
+
+            </Box>
+            </SlideFade>
+      </Container>
     )
 }
 
