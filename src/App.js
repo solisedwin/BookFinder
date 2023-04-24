70659687950fc32796ae6b60d89766cca3a8db41
solisedwin10 <solisedwin10@gmail.com>
2023-04-23 22:36:17 -0400

    Added dashboard component as a page route. Using react-router. Going to be used for testing to see if link renders new Dashboard page after successful register/login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard'
+
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
         <Switch>
             <Route path="/" component={Home} exact />
+            <Route path="/Dashboard" component={Dashboard} exact />
         </Switch>
     )
 }
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Text} from "@chakra-ui/react"
+
+
+const Dashboard = () => {
+
+    return (
+        <Text>Hello World from Dashboard</Text>
+    )
+
+}
+
+
+export default Dashboard;
